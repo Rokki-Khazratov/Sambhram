bf971114a291d07adbe3b21cba01e685da87c8a7
Shamshodbek Khazratov <marco1131mc@gmail.com>
2023-07-30 00:25:04 +0500

    add drf CRUD

--- api/views.py
+++ api/views.py
@@ -1,49 +1,19 @@
-import random
-import string
-from rest_framework import generics,status
-from rest_framework.response import Response
-from .models import Message,Content
-from .serializers import MessageSerializer,ContentSerializer
+from rest_framework import generics
+from .models import Job, Application
+from .serializers import JobSerializer, ApplicationSerializer
 
+class JobListCreateAPIView(generics.ListCreateAPIView):
+    queryset = Job.objects.all()
+    serializer_class = JobSerializer
 
-class MessageListCreateView(generics.ListCreateAPIView):
-    queryset = Message.objects.all()
-    serializer_class = MessageSerializer
+class JobRetrieveUpdateAPIView(generics.RetrieveUpdateAPIView):
+    queryset = Job.objects.all()
+    serializer_class = JobSerializer
 
-    def generate_random_slug(self):
-        length = 8
-        chars = string.ascii_letters + string.digits
-        slug = ''.join(random.choice(chars) for _ in range(length))
-        return slug
+class ApplicationListCreateAPIView(generics.ListCreateAPIView):
+    queryset = Application.objects.all()
+    serializer_class = ApplicationSerializer
 
-    def perform_create(self, serializer):
-        slug = self.generate_random_slug()
-        serializer.save(slug=slug)
-
-class MessageRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
-    queryset = Message.objects.all()
-    serializer_class = MessageSerializer
-    lookup_field = 'slug'
-
-
-class MarkAsReadView(generics.UpdateAPIView):
-    queryset = Message.objects.all()
-    serializer_class = MessageSerializer
-    lookup_field = 'pk' 
-
-    def update(self, request, *args, **kwargs):
-        instance = self.get_object()
-        instance.is_read = True
-        instance.save()
-        serializer = self.get_serializer(instance)
-        return Response(serializer.data, status=status.HTTP_200_OK)
-
-
-class ContentListCreateView(generics.ListCreateAPIView):
-    queryset = Content.objects.all()
-    serializer_class = ContentSerializer
-
-class ContentRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
-    queryset = Content.objects.all()
-    serializer_class = ContentSerializer
-    lookup_field = 'slug'
+class ApplicationRetrieveUpdateAPIView(generics.RetrieveUpdateAPIView):
+    queryset = Application.objects.all()
+    serializer_class = ApplicationSerializer
